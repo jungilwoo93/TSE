d4727b06a4992431d085b1ef216a1bc9b4f6aa6a
Andy Chabalier <Andy@chabalier.com>
2018-10-23 08:48:53 +0200

    standalone project implemented. jar could be generated and used

---
+++ TSE_GUI/src/fr/uha/ensisa/projet/pan_chabalier/controller/GeneratorController.java
@@ -0,0 +1,14 @@
+package fr.uha.ensisa.projet.pan_chabalier.controller;
+
+
+public class GeneratorController {
+
+//// do this only once per application
+//Injector injector = new MyDslStandaloneSetup().createInjectorAndDoEMFRegistration();
+// 
+//// obtain a resourceset from the injector
+//XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+// 
+//// load a resource by URI, in this case from the file system
+//Resource resource = resourceSet.getResource(URI.createFileURI("./mymodel.mydsl"), true);
+}
